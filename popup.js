270309de0a781906b15375b13ef09d403b66ceb5
davidsennin <davidfmg@outlook.com>
2016-08-31 16:08:31 -0300

    Update and rename popup.css to popup.js

---
+++ popup.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function() {
+	
+	function popup(path){
+		window.open(path,'popup','width=auto,height=auto,scrolling=auto,top=0,left=0');
+	}
+
+
+	popup('http://www.pandoapps.com.br/img/logo_low.png')
+
+});
